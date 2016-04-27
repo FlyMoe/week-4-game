424c410521e5a7bf3570d0989235956aaf7bf7ea
FlyMoe <rrgarcia@gmail.com>
2016-04-27 13:33:13 -0400

    Made some changes to logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,113 +15,113 @@ var row2_img4;
 var attackPower = 0;
 var counterAttackPower = 0;
 
+var clickedImage;
+
 // Javascript function that wraps everything
 $(document).ready(function(){
 
+	// Hide all row 2 and 3 images at the start.
+	// They will be shown later as the user clicks on images.
 	$("#row2_img1").hide();
 	$("#row2_img2").hide();
 	$("#row2_img3").hide();
 	$("#row2_img4").hide();
-
-	// ******** Row 1 *******
-	// Click Img 1, hide the rest of the images
-	$("#row1_img1").on("click", function(){
-		$("#row1_img2").hide();
-		$("#row1_img3").hide();
-		$("#row1_img4").hide();
-		$("#row2_img1").hide();	
-		$("#row2_img2").show();
-		$("#row2_img3").show();
-		$("#row2_img4").show();
-		$("#row1_img1").off("click");
-		row1_img1 = true;
-	});
-	// Click Img 2, hide the rest of the images
-	$("#row1_img2").on("click", function(){
-		$("#row1_img1").hide();
-		$("#row1_img3").hide();
-		$("#row1_img4").hide();
-		$("#row2_img2").hide();
-		$("#row2_img1").show();
-		$("#row2_img3").show();
-		$("#row2_img4").show();	
-		$("#row1_img2").off("click");
-		row1_img2 = true;	
-	});
-	// Click Img 3, hide the rest of the images
-	$("#row1_img3").on("click", function(){
-		$("#row1_img1").hide();
-		$("#row1_img2").hide();
-		$("#row1_img4").hide();
-		$("#row2_img3").hide();
-		$("#row2_img1").show();
-		$("#row2_img2").show();
-		$("#row2_img4").show();
-		$("#row1_img3").off("click");
-		row1_img3 = true;
-	});
-	// Click Img 4, hide the rest of the images
-	$("#row1_img4").on("click", function(){
-		$("#row1_img1").hide();
-		$("#row1_img2").hide();
-		$("#row1_img3").hide();
-		$("#row2_img4").hide();
-		$("#row2_img1").show();
-		$("#row2_img2").show();
-		$("#row2_img3").show();
-		$("#row1_img4").off("click");
-		row1_img4 = true;
-	});
-
 	$("#row3_img1").hide();
 	$("#row3_img2").hide();
 	$("#row3_img3").hide();
 	$("#row3_img4").hide();
 
-	// ******** Row 2 *******
-	//Click Img 1 in row 2
-	$("#row2_img1").on("click", function(){
-		$("#row2_img1").hide();
-		$("#row3_img1").show();
-		$("#row2_img2").off("click");
-		$("#row2_img3").off("click");
-		$("#row2_img4").off("click");
-		row2_img1 = true;
-	});
-	//Click Img 2 in row 2
-	$("#row2_img2").on("click", function(){
-		$("#row2_img2").hide();
-		$("#row3_img2").show();
-		$("#row2_img1").off("click");
-		$("#row2_img3").off("click");
-		$("#row2_img4").off("click");
-		row2_img2 = true;
-	});
-		//Click Img 3 in row 2
-	$("#row2_img3").on("click", function(){
-		$("#row2_img3").hide();
-		$("#row3_img3").show();
-		$("#row2_img1").off("click");
-		$("#row2_img2").off("click");
-		$("#row2_img4").off("click");
-		row2_img3 = true;
-	});
-	//Click Img 4 in row 2
-	$("#row2_img4").on("click", function(){
-		$("#row2_img4").hide();
-		$("#row3_img4").show();
-		$("#row2_img1").off("click");
-		$("#row2_img2").off("click");
-		$("#row2_img3").off("click");
-		row2_img4 = true;
-	});
 
-
+	$("div").on("click", function(){
+		clickedImage = $(this).data("image");
+		if (typeof clickedImage !== "undefined") {
+		//console.log("the images clicked", $(this).data("image") );
+			console.log("the images clicked: "+clickedImage);
+			switch (clickedImage) {
+				case "row1_img1":
+					$("#row1_img2").hide();
+					$("#row1_img3").hide();
+					$("#row1_img4").hide();
+					$("#row2_img1").hide();	
+					$("#row2_img2").show();
+					$("#row2_img3").show();
+					$("#row2_img4").show();
+					$("#row1_img1").off("click");
+					break;
+				case "row1_img2":
+					$("#row1_img1").hide();
+					$("#row1_img3").hide();
+					$("#row1_img4").hide();
+					$("#row2_img2").hide();
+					$("#row2_img1").show();
+					$("#row2_img3").show();
+					$("#row2_img4").show();	
+					$("#row1_img2").off("click");
+					row1_img2 = true;	
+					break;
+				case "row1_img3":
+					$("#row1_img1").hide();
+					$("#row1_img2").hide();
+					$("#row1_img4").hide();
+					$("#row2_img3").hide();
+					$("#row2_img1").show();
+					$("#row2_img2").show();
+					$("#row2_img4").show();
+					$("#row1_img3").off("click");
+					row1_img3 = true;
+					break;
+				case "row1_img4":
+					$("#row1_img1").hide();
+					$("#row1_img2").hide();
+					$("#row1_img3").hide();
+					$("#row2_img4").hide();
+					$("#row2_img1").show();
+					$("#row2_img2").show();
+					$("#row2_img3").show();
+					$("#row1_img4").off("click");
+					row1_img4 = true;
+					break;
+				case "row2_img1":
+					$("#row2_img1").hide();
+					$("#row3_img1").show();
+					$("#row2_img2").off("click");
+					$("#row2_img3").off("click");
+					$("#row2_img4").off("click");
+					row2_img1 = true;
+					break;	
+				case "row2_img2":
+					$("#row2_img2").hide();
+					$("#row3_img2").show();
+					$("#row2_img1").off("click");
+					$("#row2_img3").off("click");
+					$("#row2_img4").off("click");
+					row2_img2 = true;
+					break;	
+				case "row2_img3":
+					$("#row2_img3").hide();
+					$("#row3_img3").show();
+					$("#row2_img1").off("click");
+					$("#row2_img2").off("click");
+					$("#row2_img4").off("click");
+					row2_img3 = true;
+					break;	
+				case "row2_img4":
+					$("#row2_img4").hide();
+					$("#row3_img4").show();
+					$("#row2_img1").off("click");
+					$("#row2_img2").off("click");
+					$("#row2_img3").off("click");
+					row2_img4 = true;
+					break;	
+			}
+		}
+	});
 });
 
 function attack() {
 	var ranNumber;
-	var attackCharacter;
+	var counterAttackCharacter;
+	var hp;
 	//console.log(attributes.r2d2.hp);
 	// 	han_solo:{hp:120, ap:8, cap:20},
 	// kylo_ren:{hp:200, ap:10, cap:30},
@@ -130,17 +130,17 @@ function attack() {
 
 	// Attack power
 	if (row1_img1 == true) {
-		attackPower += attributes.han_solo.ap;	
-		attackCharacter = "Han Solo";	
+		attackPower += attributes.han_solo.ap;
+		hp = attributes.han_solo.hp;	
 	} else if (row1_img2 == true) {
 		attackPower += attributes.kylo_ren.ap;
-		attackCharacter = "Kylo Ren";	
+		hp = attributes.kylo_ren.hp;
 	} else if (row1_img3 == true) {
 		attackPower += attributes.r2d2.ap;
-		attackCharacter = "R2D2";	
+		hp = attributes.r2d2.hp;
 	} else if (row1_img4 == true) {
 		attackPower += attributes.boba_fett.ap;
-		attackCharacter = "Boba Fett";	
+		hp = attributes.boba_fett.hp;
 	}
 	console.log(attackPower);
 	// Counter Attack Power
@@ -158,9 +158,11 @@ function attack() {
 		counterAttackCharacter = "Boba Fett";
 	}
 
+	
+
 	document.getElementById("attack1").innerHTML = "You attacked "+counterAttackCharacter+" for "+attackPower+" damage";
 	document.getElementById("attack2").innerHTML = counterAttackCharacter+" attacked you back for "+counterAttackPower+" damage";
-
+	document.getElementById("hp").innerHTML = counterAttackCharacter+" attacked you back for "+counterAttackPower+" damage";
 
 }
 
